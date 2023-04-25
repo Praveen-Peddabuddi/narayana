28ee519b15beb3c918eeb9777b8808b21c986d6a
unknown <i.praveen0007@gmail.com>
2023-04-25 21:58:38 +0530

    sa

--- weather/views.py
+++ weather/views.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 import json
-import urllib.request
+import urllib.request 
+
+
+# narayana's programm
 
 @login_required(login_url='login')
 def homePage(request):
